53c0cf1a68b6c362b89f5f9c8d5b0858061865db
Robert Jaros <rjaros@finn.pl>
2017-10-23 12:49:41 +0200

    Ported to JS platform

---
+++ src/main/kotlin/com/lightningkite/kotlin/lifecycle/DisposeLifecycle.kt
@@ -0,0 +1,20 @@
+package com.lightningkite.kotlin.lifecycle
+
+import com.lightningkite.kotlin.Disposable
+
+/**
+ * Created by jivie on 6/28/16.
+ */
+class DisposeLifecycle() : LifecycleConnectable, Disposable {
+    val toRemove = ArrayList<LifecycleListener>()
+    override fun connect(listener: LifecycleListener) {
+        listener.onStart()
+        toRemove.add(listener)
+    }
+
+    override fun dispose() {
+        toRemove.forEach { it.onStop() }
+        toRemove.clear()
+    }
+
+}
